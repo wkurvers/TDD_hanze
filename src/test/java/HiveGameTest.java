4757497c8aafd0d4316e1c36065c2313343be73a
wouter <wkurvers11@gmail.com>
2019-10-01 12:12:12 +0200

    setup project

---
+++ src/test/java/HiveGameTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HiveGameTest {
+
+    @Test
+    void play() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void pass() {
+    }
+
+    @Test
+    void isWinner() {
+    }
+
+    @Test
+    void isDraw() {
+    }
+}
